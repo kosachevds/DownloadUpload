d92f6fadf0fe76c44d27d7a22a2f801871664703
kosds <Kos-dan@mail.ru>
2017-09-22 23:00:59 +0300

    Add project files.

---
+++ DownloadUpload/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using WinForms = System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace DownloadUpload
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int MinPort = 1024;
+        private const int MaxPort = 65535;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tabs_TabSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (tiDownload.IsSelected)
+                this.Title = tiDownload.Header.ToString();
+            else if (tiUpload.IsSelected)
+                this.Title = tiUpload.Header.ToString();
+        }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                Multiselect = false
+            };
+            if (fileDialog.ShowDialog() == true)
+                tbUploadFilePath.Text = fileDialog.FileName;
+        }
+
+        private void btnFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new WinForms.FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == WinForms.DialogResult.OK)
+                tbDownloadFolder.Text = folderDialog.SelectedPath;
+        }
+
+        private bool IsValidPort(string input)
+        {
+            int port;
+            if (!Int32.TryParse(input, out port))
+                return false;
+            return port >= MinPort && port <= MaxPort;
+        }
+
+        private bool IsValidAddressPort(string ip, string port)
+        {
+            IPAddress address;
+            if (!IPAddress.TryParse(ip, out address))
+            {
+                ShowError("Invalid ip-address");
+                return false;
+            }
+            else if (!IsValidPort(port))
+            {
+                ShowError($"Invalid port. From {MinPort} to {MaxPort}");
+                return false;
+            }
+            return true;
+        }
+
+        private bool FileIsExists(string path)
+        {
+            if (File.Exists(tbUploadFilePath.Text)) 
+                return true;
+            ShowError("Invalid file path");
+            return false;
+        }
+
+        private bool FolderIsExists(string path)
+        {
+            if (Directory.Exists(tbDownloadFolder.Text))
+                return true;
+            ShowError("Invalid folder path");
+            return false;
+        }
+
+        private void StartUploading(object sender, RoutedEventArgs e)
+        {
+            var filePath = tbUploadFilePath.Text;
+            if (!FileIsExists(filePath))
+                return;
+            var ip = tbUploadIp.Text;
+            if (!IsValidAddressPort(ip, tbUploadPort.Text))
+                return;
+            var port = Int32.Parse(tbUploadPort.Text);
+            try
+            {
+                ProgressWindow.ShowUploading(ip, port, filePath);
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+        }
+
+        private void StartDownloading(object sender, RoutedEventArgs e)
+        {
+            var folderPath = tbDownloadFolder.Text;
+            if (!FolderIsExists(folderPath))
+                return;
+            var ip = tbDownloadIp.Text;
+            if (!IsValidAddressPort(ip, tbDownloadPort.Text))
+                return;
+            var port = Int32.Parse(tbDownloadPort.Text);
+            try
+            {
+                ProgressWindow.ShowDownloading(ip, port, folderPath);
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.Message);
+            }
+        }
+
+        private static void ShowError(string content)
+        {
+            MessageBox.Show(content, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ DownloadUpload/ProgressWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DownloadUpload
+{
+    /// <summary>
+    /// Interaction logic for ProgressWindow.xaml
+    /// </summary>
+    public partial class ProgressWindow : Window
+    {
+        private BackgroundWorker _uploader;
+        private BackgroundWorker _downloader;
+        private string _ip;
+        private int _port;
+        private string _path;
+        
+        private ProgressWindow(string ip, int port, string path)
+        {
+            InitializeComponent();
+            _ip = ip;
+            _port = port;
+            _path = path;
+            _downloader = this.FindResource("downloader") as BackgroundWorker;
+            _downloader.DoWork += Download;
+            _downloader.ProgressChanged += ShowProgress;
+            _downloader.RunWorkerCompleted += CloseWindow;
+            _downloader.WorkerReportsProgress = true;
+            _uploader = this.FindResource("uploader") as BackgroundWorker;
+            _uploader.DoWork += Upload;
+            _uploader.ProgressChanged += ShowProgress;
+            _uploader.RunWorkerCompleted += CloseWindow;
+            _uploader.WorkerReportsProgress = true;
+        }
+
+        private void ShowProgress(object sender, ProgressChangedEventArgs e)
+        {
+            progress.Value = e.ProgressPercentage;
+            var span = (TimeSpan)e.UserState;
+            lblTime.Content = span.ToString();
+        }
+
+        private void CloseWindow(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                this.Close();
+            }
+            else
+            {
+                tbxError.Visibility = Visibility.Visible;
+                tbxError.Text = e.Error.Message;
+            }
+        }
+
+        public static void ShowDownloading(string ip, int port, string folder)
+        {
+            var window = new ProgressWindow(ip, port, folder);
+            window.Show();
+            window._downloader.RunWorkerAsync();
+        }
+
+        private void Download(object sender, DoWorkEventArgs e)
+        {
+            using (var client = new TcpClient())
+            {
+                client.Connect(_ip, _port);
+                using (var stream = client.GetStream())
+                using (var reader = new BinaryReader(stream))
+                {
+                    var filename = reader.ReadString();
+                    var fileSize = reader.ReadInt64();
+                    var chunkSize = (int)Math.Min(fileSize / 100, Int32.MaxValue);
+
+                    using (var file = File.Open(Path.Combine(_path, filename), FileMode.Create))
+                    {
+                        byte[] chunk = new byte[chunkSize];
+                        long size = 0L;
+                        int count;
+                        var start = DateTime.Now;
+                        while ((count = stream.Read(chunk, 0, chunkSize)) != 0)
+                        {
+                            file.Write(chunk, 0, count);
+                            size += count;
+                            var span = DateTime.Now - start;
+                            _downloader.ReportProgress((int)(size * 100 / fileSize), span);
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        public static void ShowUploading(string ip, int port, string filePath)
+        {
+            var window = new ProgressWindow(ip, port, filePath);
+            window.Show();
+            window._uploader.RunWorkerAsync();
+        }
+
+        private void Upload(object sender, DoWorkEventArgs e)
+        {
+            var filename = Path.GetFileName(_path);
+            var fileSize = new FileInfo(_path).Length;
+            var listener = new TcpListener(IPAddress.Parse(_ip), _port);
+            listener.Start();
+            using (var client = listener.AcceptTcpClient())
+            using (var stream = client.GetStream())
+            using (var writer = new BinaryWriter(stream))
+            {
+                writer.Write(filename);
+                writer.Write(fileSize);
+                var chunkSize = (int)Math.Min(fileSize / 100, Int32.MaxValue);
+                using (var file = File.Open(_path, FileMode.Open))
+                {
+                    byte[] chunk = new byte[chunkSize];
+                    long size = 0L;
+                    int count;
+                    var start = DateTime.Now;
+                    while ((count = file.Read(chunk, 0, chunkSize)) != 0)
+                    {
+                        stream.Write(chunk, 0, count);
+                        size += count;
+                        var span = DateTime.Now - start;
+                        _uploader.ReportProgress((int)(size * 100 / fileSize), span);
+                    }
+                }
+            }
+            listener.Stop();
+        }
+
+    }
+}
